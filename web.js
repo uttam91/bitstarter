ef905ae7d514eddec826d31cc8a1e00ad8ca7489
uttam6561@gmail.com <uttam@linux-v37k.site>
2013-07-10 01:31:34 +0530

    updated ver 3 web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(fs.readFileSync('index.html'),"utf-8"); 
 
 app.get('/', function(request, response) {
-    response.send(buf.tostring());
+    response.send(buf.tostring('utf8',0,buffer));
 });
 
 var port = process.env.PORT || 5000;
