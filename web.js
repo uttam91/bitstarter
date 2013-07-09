5880c04e2c2e9a48735b702a7f04e458cfc17852
uttam6561@gmail.com <uttam@linux-v37k.site>
2013-07-10 01:44:50 +0530

    updated ver 5 web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFileSync('index.html')); 
+var buffer = readFileSync('index.html'); 
 
 app.get('/', function(request, response) {
     response.send(buf.tostring('utf8',buffer));
